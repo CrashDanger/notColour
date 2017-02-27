2dad3112deff82c66f363b2364fdd442ef1dd1df
CrashDanger <tylerjevans@gmail.com>
2017-02-27 09:17:50 -0800

    Initial Commit. Basic functionality works

---
+++ notColour/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace notColour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.CommunicationBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(93, 104);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(95, 23);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Chose Image";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            // 
+            // CommunicationBox
+            // 
+            this.CommunicationBox.Location = new System.Drawing.Point(93, 146);
+            this.CommunicationBox.Name = "CommunicationBox";
+            this.CommunicationBox.ReadOnly = true;
+            this.CommunicationBox.Size = new System.Drawing.Size(94, 20);
+            this.CommunicationBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.CommunicationBox);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.TextBox CommunicationBox;
+    }
+}
+
---
+++ notColour/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notColour
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap workingImage;
+        public Form1()
+        {
+            InitializeComponent();
+            OpenButton.Click += OpenButton_Click;
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openDialog = new OpenFileDialog())
+            {
+                if (openDialog.ShowDialog() == DialogResult.OK)
+                {
+                    workingImage = new Bitmap(openDialog.FileName);
+                    for (int x = 0; x < workingImage.Width; x++)
+                    {
+                        for (int y = 0; y < workingImage.Height; y++)
+                        {
+                            var pixel = workingImage.GetPixel(x, y);
+                            int average = (int)(pixel.R*0.21 + pixel.G*0.72 + pixel.B*0.07);
+                            Color BWpixel = Color.FromArgb(average, average, average);
+                            workingImage.SetPixel(x,y,BWpixel);
+                        }
+                    }
+                    SaveFileDialog tofinish = new SaveFileDialog();
+                    tofinish.Filter += "Same as Original | *" + getExtension(openDialog.FileName);
+                    if (tofinish.ShowDialog() == DialogResult.OK)
+                    {
+                        workingImage.Save(tofinish.FileName);
+                        CommunicationBox.Text = "success";
+                    } else { CommunicationBox.Text = "failure"; }
+                }
+            }
+        }
+
+        private string getExtension(string path)
+        {
+            List<char> preresult = new List<char>();
+            int l = path.Length - 1;
+            for (int i = l; i > 0; i--)
+            {
+                preresult.Insert(0, path[i]);
+                if (path[i] == '.') break;
+            }
+            return new string(preresult.ToArray());
+        }
+    }
+}
